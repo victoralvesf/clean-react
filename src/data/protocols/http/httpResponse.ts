39a7db8145915f0e3899267a465a9de77db9aba4
Victor Alves <victoralves@fortics.com.br>
2020-09-03 19:58:41 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on default error cases

--- src/data/protocols/http/httpResponse.ts
+++ src/data/protocols/http/httpResponse.ts
@@ -1,6 +1,10 @@
 export enum HttpStatusCode {
+  success = 200,
   noContent = 204,
-  unauthorized = 401
+  badRequest = 400,
+  unauthorized = 401,
+  notFound = 404,
+  serverError = 500
 }
 
 export type HttpResponse = {
--- src/data/usecases/authentication/remoteAuthentication.ts
+++ src/data/usecases/authentication/remoteAuthentication.ts
@@ -2,6 +2,7 @@ import { HttpPostClient } from '@/data/protocols/http/httpPostClient'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 import { InvalidCredentialsError } from '@/domain/errors/invalidCredentialsError'
+import { UnexpectedError } from '@/domain/errors/unexpectedError'
 
 export class RemoteAuthentication {
   constructor (
@@ -15,7 +16,9 @@ export class RemoteAuthentication {
       body: params
     })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.success: break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
     }
   }
 }
